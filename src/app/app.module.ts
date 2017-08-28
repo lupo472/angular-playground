2b05edbe2455b1a7a992ad0bc014243df4046d54
lantilici <luca.antilici@aubay.it>
2017-08-28 11:25:45 +0200

    Lesson 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SimpleFormComponent } from './simple-form/simple-form.component';
 import {MailService} from './mail.service';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import {MailService} from './mail.service';
     SimpleFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [MailService],
   bootstrap: [AppComponent]
